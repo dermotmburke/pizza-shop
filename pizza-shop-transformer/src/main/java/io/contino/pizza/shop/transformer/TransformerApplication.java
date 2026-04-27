caf8f97ffdff74ad3676b6ac25412e2a87fd86c9
dermot.burke <dermot.burke@defra.gov.uk>
2026-04-27 15:16:59 +0100

    upgrade from java 17 to 25

--- pizza-shop-transformer/src/main/java/io/contino/pizza/shop/transformer/TransformerApplication.java
+++ pizza-shop-transformer/src/main/java/io/contino/pizza/shop/transformer/TransformerApplication.java
@@ -4,11 +4,9 @@ import io.contino.pizza.shop.config.TracingConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.boot.test.autoconfigure.actuate.observability.AutoConfigureObservability;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@AutoConfigureObservability
 @ConfigurationPropertiesScan("io.contino.pizza.shop")
 @Import({TracingConfig.class})
 public class
